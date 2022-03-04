05a47db850b03304c60e11b608395e76fcd1cb4c
anuragchitrakar <57140829+anuragchitrakar@users.noreply.github.com>
2022-03-05 00:49:08 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+# Add Implementation 
+def add(x,y):
+	return x+y
+
+# subtract Implementation 
+def subtract(x,y):
+	pass
+
+# multiply Implementation 
+def multiply(x,y):
+	pass
+
+# divide Implementation 
+def divide(x,y):
+	pass
